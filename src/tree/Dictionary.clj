8eb4207bb4b5d2825673800aee8b2f713d8d7017
apploid <apploidyakutsk@gmail.com>
2021-12-21 14:55:29 +0300

    update

--- src/tree/Dictionary.clj
+++ src/tree/Dictionary.clj
@@ -1,34 +1,36 @@
 (ns tree.Dictionary)
 
+(use
+ 'clojure.test)
+
+;left - less, right - greater
+
 (defn make-node
-  ([less greater value payload]
-   {:less less :greater greater :value value :payload payload})
-  ([value payload]
-   (make-node nil nil value payload)))
+  ([left right key value]
+   {:left left :right right :key key :value value})
+  ([key value]
+   (make-node nil nil key value)))
 
 (defn comparator-over-nodes
   "Returns a comparison function specific for values of tree nodes"
   ([func]
    (fn [node1 node2]
-     (func (:value node1) (:value node2))))
+     (func (:key node1) (:key node2))))
   ([func & args]
    (comparator-over-nodes #(func args %))))
 
-(def equals
-  (comparator-over-nodes =))
-
-(def greater-than
+(def right-than
   (comparator-over-nodes >))
 
-(def less-than
-  (comparator-over-nodes <))
+(def equals
+  (comparator-over-nodes =))
 
 (defn height
   "Determines the depth of the BST"
   ([root count]
    (if root
-     (max (height (:less root) (inc count))
-          (height (:greater root) (inc count)))
+     (max (height (:left root) (inc count))
+          (height (:right root) (inc count)))
      count))
   ([root]
    (height root 0)))
@@ -38,154 +40,199 @@
   [root new-node]
   (if (nil? root)
     new-node
-    (if (greater-than new-node root)
-      (assoc root :greater (insert-node (:greater root) new-node))
+    (if (right-than new-node root)
+      (assoc root :right (insert-node (:right root) new-node))
 
       (if (equals new-node root)
-        (assoc root :payload (:payload new-node))
-        (assoc root :less (insert-node (:less root) new-node)))
-
-      )))
+        (assoc root :value (:value new-node))
+        (assoc root :left (insert-node (:left root) new-node))))))
 
 (defn find-node
   "find node in BST"
   [root key]
-  (if (nil? (:value root))
+  (if (nil? (:key root))
     nil
-    (if (= key (:value root))
+    (if (= key (:key root))
       root
-      (if (> key (:value root))
-        (find-node (:greater root) key)
-        (find-node (:less root) key)
-        )
-      )
-    ))
+      (if (> key (:key root))
+        (find-node (:right root) key)
+        (find-node (:left root) key)))))
 
-(defn find-less-node
-  "find node in BST"
+(defn find-smallest-node
   [root]
-  (if (nil? (:less root))
+  (if (nil? (:left root))
     root
-    (find-less-node (:less root))))
+    (find-smallest-node (:left root))))
 
 
 (defn find-node-parent
   "find parent-node in BST"
   ([root key parent]
-   (if (nil? (:value root))
+   (if (nil? (:key root))
      nil
-     (if (= key (:value root))
+     (if (= key (:key root))
        {:parent parent :child root}
-       (if (> key (:value root))
-         (find-node-parent (:greater root) key root)
-         (find-node-parent (:less root) key root)
-         )
-       )
-     ))
+       (if (> key (:key root))
+         (find-node-parent (:right root) key root)
+         (find-node-parent (:left root) key root)))))
   ([root key]
-   (find-node-parent root key nil))
-  )
+   (find-node-parent root key nil)))
 
-(defn add-less-node
-  [less-node root]
+(defn add-left-node
+  [left-node root]
   (if (nil? root)
-    less-node
-    (assoc (find-less-node root) :less less-node)
-    )
+    left-node
+    (assoc (find-smallest-node root) :left left-node)))
 
-  )
-
-(defn -remove-child
+(defn remove-child
   [e]
+  ; {:parent parent :child root} object
   (if (nil? (:parent e))
-    (if (nil? (:greater (:child e) ) )
-      (:less (:child e) )
-      (add-less-node (:less (:child e)) (:greater (:child e)))
-      )
-    (if (greater-than (:child e) (:parent e))
-      (assoc (:parent e) :greater (add-less-node (:less (:child e)) (:greater (:child e) )))
-      (assoc (:parent e) :less (add-less-node (:less (:child e)) (:greater (:child e) ))))
-    ))
+    (if (nil? (:right (:child e)))
+      (:left (:child e))
+      (add-left-node (:left (:child e)) (:right (:child e))))
+    (if (right-than (:child e) (:parent e))
+      (assoc (:parent e) :right (add-left-node (:left (:child e)) (:right (:child e))))
+      (assoc (:parent e) :left (add-left-node (:left (:child e)) (:right (:child e)))))))
 
 
 (defn remove-node
   [root remove-key]
-  (-remove-child (
-                   find-node-parent root remove-key))
-
-  )
-
-
-
-(defn bld-rand-tree
-  "Creates a BST of depth target-height full of random floats"
-  ([target-height]
-   (bld-rand-tree target-height (make-node (rand) (rand))))
-  ([target-height root]
-   (if (>= target-height (height root))
-     (bld-rand-tree target-height (insert-node root (make-node (rand) (rand))))
-     root)))
+  (remove-child
+   (find-node-parent root remove-key)))
 
 (defn greatest
   [root]
-  (if (:greater root)
-    (greatest (:greater root))
+  (if (:right root)
+    (greatest (:right root))
     root))
 
-(defn is-bst
-  "Not only does this check to see if the value at each 'less' child of
-  each node is less than the value at that node (and 'greater'), but it also
-  checks that EVERY element to the 'less' of each node is less than that node.
-  Otherwise, the tree is not a BST. Additionally, this function ensures that "
-  ([root greaters lesss]
-   (if root
-     (every? identity
-             [(if (every? (fn [a-min] (< (:value root) a-min)) lesss)
-                (is-bst (:less root) greaters (conj lesss (:value root)))
-                false)
-              (if (every? (fn [a-max] (> (:value root) a-max)) greaters)
-                (is-bst (:greater root) (conj greaters (:value root)) lesss)
-                false)])
-     true))
-  ([root]
-   (is-bst root [] [])))
-
 (def my-tree
-  (assoc (assoc (assoc (make-node 10 666)
-                  :less (make-node 8 666))
-           :greater (make-node 11 666))
-    :less (make-node 4 666)))
+  (assoc
+   (assoc
+    (assoc (make-node 10 666)
+           :left (make-node 8 666))
+    :right (make-node 11 666))
+   :left (make-node 4 666)))
 
 (defn add-to-dict
   ([dict entry]
-   (insert-node dict (make-node (:key entry) (:value entry))))
+   (if (nil? entry)
+     dict
+     (insert-node dict (make-node (:key entry) (:value entry)))))
 
   ([entry]
-   (insert-node nil (make-node (:key entry) (:value entry))))
-  )
+   (insert-node nil (make-node (:key entry) (:value entry)))))
 
 (defn value-from-dict
   [dict key]
-  (:payload (find-node dict key))
-  )
+  (:value (find-node dict key)))
 
 
 (defn create-dict
   [& args]
-  (apply add-to-dict args)
-  )
+  (apply add-to-dict args))
 
 
-(
-  defn -main
-  "Demo this thing"
+(defn -main
   [& args]
-  ;(println (str "Is my-tree a BST? " (hip-speech (is-bst my-tree))))
-  ;(println (str "Well what about rand-bst? " (hip-speech (is-bst rand-bst))))
 
-  ;(println (insert-node nil (make-node 10 666)))
-  (println (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}))
-  (println (
-             remove-node
-             (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}) 666)))
+  (println
+   (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}))
+  (println
+   (remove-node
+    (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}) 666)))
+
+
+(def my-tree
+  (assoc
+   (make-node 666 100)
+   :left (make-node 111 1600)))
+
+
+;(-main)
+
+
+; TEST ------------------------------------
+
+(deftest create-dict-test
+  (is
+   (=
+    {:left nil, :right nil, :key 666, :value 777}
+    (create-dict {:key 666, :value 777}))))
+
+(deftest create-nil-dict-test
+  (is
+   (=
+    {:left nil, :right nil, :key nil, :value nil}
+    (create-dict nil))))
+
+(deftest add-to-dict-test
+  (is
+   (=
+    {:left
+     {:left nil, :right nil,
+      :key  111, :value 1500},
+     :right nil,
+     :key   666,
+     :value 100}
+    (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}))))
+
+(deftest add-to-nil-test
+  (is
+   (=
+    {:left nil, :right nil,
+     :key  666, :value 100}
+    (add-to-dict nil {:key 666 :value 100}))))
+
+(deftest add-nil-to-dict-test
+  (is
+   (=
+    {:left nil, :right nil,
+     :key  555, :value 0}
+    (add-to-dict (create-dict {:key 555 :value 0}) nil))))
+
+(deftest add-existing-value-test
+  (is
+   (=
+    {:left
+     {:left nil, :right nil,
+      :key  111, :value 1500},
+     :right nil,
+     :key   666,
+     :value -20}
+    (add-to-dict (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}) {:key 666 :value -20})
+     )))
+
+(deftest get-value-test
+  (is
+   (=
+    1500
+    (value-from-dict (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}) 111)
+     )))
+
+
+(deftest non-existing-value
+  (is
+   (=
+    nil
+    (value-from-dict (add-to-dict (create-dict {:key 666 :value 100}) {:key 111 :value 1500}) 0)
+    )))
+
+(create-dict-test)
+
+(create-nil-dict-test)
+
+(add-to-nil-test)
+
+(add-to-dict-test)
+
+(add-nil-to-dict-test)
+
+(add-existing-value-test)
+
+(get-value-test)
+
+(non-existing-value)
+
 
